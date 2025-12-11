009d4b0adce5595acb1503129063e3efb6489960
Nezudevv <mbreaux@freightwisellc.com>
2025-12-10 20:19:49 -0600

    vuejs working, not perfect though

---
+++ .config/nvim/lua/nezudevv/plugins/nvim-lspconfig.lua
@@ -0,0 +1,158 @@
+return {
+	-- Main LSP Configuration
+	"neovim/nvim-lspconfig",
+
+	dependencies = {
+		-- Automatically install LSPs and related tools to stdpath for Neovim
+		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+		-- Useful status updates for LSP.
+		{ "j-hui/fidget.nvim", opts = { notification = { window = { winblend = 0 } } } },
+
+		-- Allows extra capabilities provided by nvim-cmp
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		--  This function gets run when an LSP attaches to a particular buffer.
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+
+				-- Jump to the definition of the word under your cursor.
+				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+				-- Find references for the word under your cursor.
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+				-- Jump to the implementation of the word under your cursor.
+				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+				-- Jump to the type of the word under your cursor.
+				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+				-- Fuzzy find all the symbols in your current document.
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+				-- Fuzzy find all the symbols in your current workspace.
+				map(
+					"<leader>ws",
+					require("telescope.builtin").lsp_dynamic_workspace_symbols,
+					"[W]orkspace [S]ymbols"
+				)
+
+				-- Rename the variable under your cursor.
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				-- Execute a code action
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+				-- Goto Declaration
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+				-- Highlight references of the word under your cursor
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+			end,
+		})
+
+		-- LSP servers and clients are able to communicate to each other what features they support.
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+		-- Enable the following language servers
+		local servers = {
+			vue_ls = {}, -- Will be configured via custom handler
+			ts_ls = {}, -- Will be configured via custom handler
+			lua_ls = {
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						-- diagnostics = { disable = { 'missing-fields' } },
+					},
+				},
+			},
+		}
+
+		-- Ensure the servers and tools above are installed
+		require("mason").setup()
+
+		-- You can add other tools here that you want Mason to install
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua", -- Used to format Lua code
+			"typescript-language-server", -- ts_ls for TypeScript/JavaScript
+			"vue-language-server", -- Volar for Vue
+		})
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		-- Setup ts_ls with Vue TypeScript plugin (pointing to global npm installation)
+		require("lspconfig").ts_ls.setup({
+			on_attach = function(client, bufnr)
+				-- Trigger LspAttach event for keymaps
+				vim.api.nvim_exec_autocmds("User", { pattern = "LspAttach" })
+			end,
+			capabilities = capabilities,
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vim.fn.trim(vim.fn.system("npm root -g")) .. "/@vue/language-server",
+						languages = { "vue" },
+					},
+				},
+			},
+			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+		})
+
+		-- Setup volar with default config
+		require("lspconfig").volar.setup({
+			capabilities = capabilities,
+		})
+
+		-- Setup other servers via mason-lspconfig
+		require("mason-lspconfig").setup({
+			handlers = {
+				-- Default handler for most servers
+				function(server_name)
+					-- Skip ts_ls and vue_ls since we already set them up above
+					if server_name == "ts_ls" or server_name == "vue_ls" then
+						return
+					end
+					local server = servers[server_name] or {}
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+	end,
+}
